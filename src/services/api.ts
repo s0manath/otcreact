58bd5f3567f4c7468bcf7d1b97c50aebc7825f33
s0manath <soma756126@gmail.com>
2026-04-17 01:20:55 +0530

    chore: remove encryption and decryption from axios interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { encrypt, decrypt } from "../utils/crypto-utils";
 
 const api = axios.create({
   baseURL: "http://localhost:5218/api", // Correct port from launchSettings.json
@@ -14,39 +13,14 @@ api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
-  // Encrypt POST request body
-  if (config.method === "post") {
-    config.data = { encryptedata: encrypt(config.data || {}) };
-  }
-
   return config;
 });
 
 api.interceptors.response.use(
   (response) => {
-    // Decrypt response if it contains encryptedata
-    if (response.data && response.data.encryptedata) {
-      try {
-        response.data = decrypt(response.data.encryptedata);
-      } catch (error) {
-        console.error("Decryption failed:", error);
-      }
-    }
     return response;
   },
   (error) => {
-    // Handle response with encrypted error data if needed
-    if (
-      error.response &&
-      error.response.data &&
-      error.response.data.encryptedata
-    ) {
-      try {
-        error.response.data = decrypt(error.response.data.encryptedata);
-      } catch (decError) {
-        console.error("Error decryption failed:", decError);
-      }
-    }
     return Promise.reject(error);
   },
 );
